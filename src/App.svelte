add5a81488abdd9fbf1514a2191e545970fa3659
Divkov575 <dmitriy@ivkov.net>
2024-07-21 13:05:28 -0400

    'init'

---
+++ src/App.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+</script>
+
+<main>
+
+    <div id="d1">
+        <div id="d1-inner">
+            <h1>Belmont School of <span>Code<div style="background-color: white; display: inline-block; height: 0.8rem; width: 0.8rem; border-radius: 6%; margin-left: 1px;"></div></span></h1>
+        </div>
+    </div>
+
+    <div id="d2">
+        <div id="d2-inner">
+            <a><h2><a id="learn-box">Learn</a>to code!</h2></a>
+            <div style="height: 0.75rem"></div>
+            <h3>BSC is a non-profit organization, devoted towards creating a computer science education, accessible to everyone</h3>
+            <h3>BSC offers </h3>
+
+        </div>
+    </div>
+
+    <div id="d3">
+        <div id="d3-inner">
+            <a><h2>BSC Boston 2024</h2></a>
+            <div style="height: 0.4rem"></div>
+            <h3>2 free seminars on July 13-14 from 6:00-7:30 PM</h3>
+            <h3>Attending both is encouraged. Topics include: Web design with Google Sites and Wix, internet and website architecture, building websites in HTML/CSS, and publishing website with Vercel</h3>
+
+            <a href=""><div class="button1">Register</div></a>
+        </div>
+    </div>
+
+    <div id="d4">
+        <div id="d4-inner">
+            <a><h2>Learn Python Now</h2></a>
+            <div style="height: 0.2rem"></div>
+
+            <h3>Affordable Python classes for middle and high-schoolers. Proceedings go towards charity. Course completion time varies.</h3>
+            <div style="height: 0.2rem"></div>
+
+            <ul>
+                <li><h3>Variables, operators, conditions, Loops, builtin Data-structures</h3></li>
+                <li><h3>OOP, Decorators, Lambda, Error handling</h3></li>
+                <li><h3>Students choice of Website development, Games, Data-Science or Algorithms</h3></li>
+            </ul>
+
+            <a href=""><div class="button1">Register</div></a>
+        </div>
+    </div>
+
+</main>
+
+<style>
+</style>
