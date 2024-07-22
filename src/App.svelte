3ec39e1bc792fb5cf2ae8769e1d8e0c813d74415
Divkov575 <dmitriy@ivkov.net>
2024-07-21 22:47:12 -0400

    'added open graph protocol support after much trial an error'

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import Countup from './lib/Countup.svelte'
+    import {fly} from 'svelte/transition';
+    import Countup from './lib/Countup.svelte';
 
     let visible = true;
     const gmailClick = () => {
@@ -7,11 +8,17 @@
             visible = false;
         }, 15000)
     }
+
+    let show_d2 = true;
+    let show_d3 = true;
+    let show_d4 = true;
+    let show_d5 = true;
 </script>
 
 <main>
 <!--    https://github.com/DIvkov575/BSC2.3-->
 
+    <button style="width: 500px" on:click={() => {show_d2 = !show_d2; show_d3 = !show_d3; show_d4 = !show_d4; show_d5 = !show_d5}}></button>
 
     <div id="d1">
         <img src="/bg2.png" alt="generic bg">
@@ -20,26 +27,29 @@
         </div>
     </div>
 
-    <div class="full-width-container"  id="d2">
-        <h1 style="color: black">BSC</h1>
-        <div style="height: 0.5rem"></div>
-        <h3>BSC is a non-profit organization, devoted to creating a computer science education, accessible to everyone.</h3>
-        <h3>BSC offers seminars and classes, with proceedings going towards improving the BSC content and education charities in Boston</h3>
+    {#if show_d2}
+        <div transition:fly={{x: -200, duration: 400}} class="full-width-container"  id="d2">
+            <h1 style="color: black">BSC</h1>
+            <div style="height: 0.5rem"></div>
+            <h3>BSC is a non-profit organization, devoted to creating a computer science education, accessible to everyone.</h3>
+            <h3>BSC offers seminars and classes, with proceedings going towards improving the BSC content and education charities in Boston</h3>
 
-        <div id="counters">
-            <div class="box"><h3><Countup value={15}/>+ Alumni</h3></div>
-            <div class="box"><h3>$<Countup value={2500}/>+ donated</h3></div>
+            <div id="counters">
+                <div class="box"><h3><Countup value={15}/>+ Alumni</h3></div>
+                <div class="box"><h3>$<Countup value={2500}/>+ donated</h3></div>
+            </div>
+
+            <div id="npo_logos">
+                <div class="logo-wrapper"><img src="/gwc_logo.png" alt="gwc_logo"></div>
+                <div class="logo-wrapper"><img src="/BEDF_logo.jpeg" alt="Boston Education Fund logo"></div>
+                <div class="logo-wrapper"><img src="/bgc_logo.png" alt="black girls who code logo"></div>
+            </div>
         </div>
+    {/if}
 
-        <div id="npo_logos">
-            <div class="logo-wrapper"><img src="/gwc_logo.png" alt="gwc_logo"></div>
-            <div class="logo-wrapper"><img src="/BEDF_logo.jpeg" alt="Boston Education Fund logo"></div>
-            <div class="logo-wrapper"><img src="/bgc_logo.png" alt="black girls who code logo"></div>
-        </div>
 
-    </div>
-
-        <div class="full-width-container" id="d3">
+    {#if show_d3}
+        <div transition:fly={{x:-250, duration: 400}} class="full-width-container" id="d3">
             <div class="inner-full-width-container">
                 <h1>BSC <a href="https://forms.gle/Uy89J5CXqN9rJrvi9" target="_blank" class="box" style="margin-left: 0.1rem">Boston</a> 2024</h1>
                 <div style="height: 0.75rem"></div>
@@ -55,44 +65,49 @@
                 </div>
             </a>
         </div>
+    {/if}
 
-    <div class="full-width-container" id="d4">
-        <div class="inner-full-width-container">
-            <h1><a href="https://forms.gle/T7vFU1fbsx5aQjJs8" target="_blank" class="box">Learn</a>to code</h1>
-            <div style="height: 0.75rem"></div>
-            <h3>Affordable Python classes for middle and high-schoolers. Proceedings go towards charity</h3>
-            <ul style="margin: 0.5rem auto 0.75rem auto; padding-left: 1.5rem">
-                <li><h3>Fundamentals: operators, conditions, Loops, etc</h3></li>
-                <li><h3>Student Choice: Web development, Games, or Data-Science</h3></li>
-            </ul>
-            <h3>$200 / 20 classes</h3>
-        </div>
-
-        <a href="https://forms.gle/T7vFU1fbsx5aQjJs8" target="_blank">
-            <div class="Qr-Wrapper">
-                <img src="/BSCPYTHONQR.png" alt="BSC-Boston-Qr-code">
-                <h3>Click Me</h3>
+    {#if show_d4}
+        <div transition:fly={{x:-400, duration: 400}} class="full-width-container" id="d4">
+            <div class="inner-full-width-container">
+                <h1><a href="https://forms.gle/T7vFU1fbsx5aQjJs8" target="_blank" class="box">Learn</a>to code</h1>
+                <div style="height: 0.75rem"></div>
+                <h3>Affordable Python classes for middle and high-schoolers. Proceedings go towards charity</h3>
+                <ul style="margin: 0.5rem auto 0.75rem auto; padding-left: 1.5rem">
+                    <li><h3>Fundamentals: operators, conditions, Loops, etc</h3></li>
+                    <li><h3>Student Choice: Web development, Games, or Data-Science</h3></li>
+                </ul>
+                <h3>$200 / 20 classes</h3>
             </div>
-        </a>
-    </div>
 
-    <div id="d5" class="full-width-container">
-        <div class="inner-full-width-container">
-            <h1><a href="https://account.venmo.com/u/Belmontcodes" target="_blank" class="box">Donate</a> Now</h1>
-            <div style="height: 0.75rem"></div>
-
-            <h3>Your funds will go towards:</h3>
-            <h3>Improving our content, making free seminars possible, site & service fees, and various charities in the Boston Area</h3>
-
+            <a href="https://forms.gle/T7vFU1fbsx5aQjJs8" target="_blank">
+                <div class="Qr-Wrapper">
+                    <img src="/BSCPYTHONQR.png" alt="BSC-Boston-Qr-code">
+                    <h3>Click Me</h3>
+                </div>
+            </a>
         </div>
+    {/if}
 
-        <a href="https://account.venmo.com/u/Belmontcodes" target="_blank" >
-            <div class="Qr-Wrapper">
-                <img src="/VenmoQr.png" alt="BSC-Boston-Qr-code">
-                <h3 style="top: -0.75rem">Click Me</h3>
+    {#if show_d5}
+        <div transition:fly={{x:-400}} id="d5" class="full-width-container">
+            <div class="inner-full-width-container">
+                <h1><a href="https://account.venmo.com/u/Belmontcodes" target="_blank" class="box">Donate</a> Now</h1>
+                <div style="height: 0.75rem"></div>
+
+                <h3>Your funds will go towards:</h3>
+                <h3>Improving our content, making free seminars possible, site & service fees, and various charities in the Boston Area</h3>
+
             </div>
-        </a>
-    </div>
+
+            <a href="https://account.venmo.com/u/Belmontcodes" target="_blank" >
+                <div class="Qr-Wrapper">
+                    <img src="/VenmoQr.png" alt="BSC-Boston-Qr-code">
+                    <h3 style="top: -0.75rem">Click Me</h3>
+                </div>
+            </a>
+        </div>
+    {/if}
 
     <div id="footer">
         <div id="footer-inner">
